f01fad53332090b9fa7e61806b1f93d4d036de90
tomoaki-kimura <tkim19750404@gmail.com>
2021-10-28 02:06:19 +0900

    fix turbo links

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,7 @@ import 'bootstrap';
 import '../stylesheets/application';
 
 require("@rails/ujs").start()
-//require("turbolinks").start()
+require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
