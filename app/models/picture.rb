2d609460770fcb9fc265989f1bb834766c4f75fb
tomoaki-kimura <tkim19750404@gmail.com>
2021-11-04 11:05:04 +0900

    add picture_validation

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,8 +2,7 @@ class Picture < ApplicationRecord
   has_one_attached :picture
   validates :title, presence: true, length: { maximum: 15 }
   validates :memo, length: { maximum: 1000 }
-  validates :picture, attached_file_type: { pattern: /^image\// },
-                      attached_file_size: { maximum: 5.megabytes } 
+  validate :image_presence
 
   def latitude
     if picture.attached?
@@ -14,6 +13,16 @@ class Picture < ApplicationRecord
   def longitude
     if picture.attached?
       picture.metadata["longitude"]
+    end
+  end
+
+  def image_presence
+    if picture.attached?
+      if !picture.content_type.in?(%('image/jpeg image/png'))
+        errors.add(:picture, 'にはjpegまたはpngファイルを添付してください')
+      end
+    else
+      errors.add(:picture, 'ファイルを添付してください')
     end
   end
 end
