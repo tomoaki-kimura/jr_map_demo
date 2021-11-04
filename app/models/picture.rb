177571c611a9ad7e058b6a517e0ce429c507d85f
tomoaki-kimura <tkim19750404@gmail.com>
2021-11-04 10:50:21 +0900

    add validation picture

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,7 +2,9 @@ class Picture < ApplicationRecord
   has_one_attached :picture
   validates :title, presence: true, length: { maximum: 15 }
   validates :memo, length: { maximum: 1000 }
-  
+  validates :picture, attached_file_type: { pattern: /^image\// },
+                      attached_file_size: { maximum: 5.megabytes } 
+
   def latitude
     if picture.attached?
       picture.metadata["latitude"]
